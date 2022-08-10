37a6f6ecd6b04bcfcea9bbebe44941bd302f0b19
SohrabFarjami <111026287+SohrabFarjami@users.noreply.github.com>
2022-08-11 02:42:31 +0300

    Add files via upload

---
+++ alarm.py
@@ -0,0 +1,25 @@
+import datetime
+from text_to_speech import speak
+
+
+print('Time management protocol activated')
+stop = False ## To loop forever
+while stop == False:
+    now = datetime.datetime.now()
+    if now.minute == 00 and now.second == 0: ##When minute and second is both 0 is it new hour
+        armyHour = now.hour
+        if armyHour == 12:
+            messageUser = ('It is now 12 pm')
+        elif armyHour == 24:
+            messsageUser = ('It is now 12 am')
+        elif armyHour > 12:
+            standardHour = armyHour - 12 
+            messageUser = ('It is now ' + str(standardHour) + 'pm')
+        else:
+            messageUser = ('It is now ' + str(armyHour) + 'am')
+        
+        speak(messageUser, "en", save=True, file="schedule.mp3")
+        
+        
+
+

dd4a99a28da85f01df6bb4080d9df072282e7b86
SohrabFarjami <111026287+SohrabFarjami@users.noreply.github.com>
2022-08-11 03:18:29 +0300

    While loop runs once a second

--- alarm.py
+++ alarm.py
@@ -1,11 +1,13 @@
 import datetime
 from text_to_speech import speak
-
-
+from time import time, sleep
+    
 print('Time management protocol activated')
 stop = False ## To loop forever
 while stop == False:
     now = datetime.datetime.now()
+    sleep(1 - time() % 1)
+    print(now.second)
     if now.minute == 00 and now.second == 0: ##When minute and second is both 0 is it new hour
         armyHour = now.hour
         if armyHour == 12:

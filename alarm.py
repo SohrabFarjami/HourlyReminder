3fdba1122c50bef93911ce698cca0df004df6220
SohrabFarjami <111026287+SohrabFarjami@users.noreply.github.com>
2022-08-14 14:16:09 +0300

    Made code more efficient

--- alarm.py
+++ alarm.py
@@ -1,25 +1,13 @@
-import datetime
+from datetime import datetime
 from text_to_speech import speak
 from time import time, sleep
     
 print('Time management protocol activated')
 stop = False ## To loop forever
 while stop == False:
-    now = datetime.datetime.now()
-    sleep(1 - time() % 1)
-    print(now.second)
-    if now.minute == 00 and now.second == 0: ##When minute and second is both 0 is it new hour
-        armyHour = now.hour
-        if armyHour == 12:
-            messageUser = ('It is now 12 pm')
-        elif armyHour == 24:
-            messsageUser = ('It is now 12 am')
-        elif armyHour > 12:
-            standardHour = armyHour - 12 
-            messageUser = ('It is now ' + str(standardHour) + 'pm')
-        else:
-            messageUser = ('It is now ' + str(armyHour) + 'am')
-        
+    now = datetime.now()
+    if now.minute == 0 and now.second == 0: ##When minute and second is both 0 its a new hour     
+        messageUser = ('It is now ' + now.strftime("%I %p"))
         speak(messageUser, "en", save=True, file="schedule.mp3")
         
         
